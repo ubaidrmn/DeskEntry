884206592519ad30639a88adc356b591c6537bff
ubaidrmn <rehmanubaid2003@gmail.com>
2025-03-19 07:03:50 +0500

    feat: Implement logic for creating desktop entries.
    Added a function `create_desktop_entry` that is called whenever the user runs the `add` command. This function creates a desktop entry.

--- src/cli.py
+++ src/cli.py
@@ -2,17 +2,39 @@ import os
 import argparse
 import settings
 
+from lib import create_desktop_entry
+
+
 def run():
     parser = argparse.ArgumentParser(
         prog=settings.PROGRAM_NAME,
         description=settings.DESCRIPTION,
-        epilog=settings.EPILOG
+        epilog=settings.EPILOG,
     )
 
-    parser.add_argument("add")
-    
+    subparsers = parser.add_subparsers(
+        help="Choose the action you want to perform.", dest="command"
+    )
+
+    # Adding entry
+
+    add_parser = subparsers.add_parser("add", help="Add a new desktop entry")
+    add_parser.add_argument("-n", "--name", required=True)
+    add_parser.add_argument("-p", "--path", required=True)
+    add_parser.add_argument("-e", "--exec", required=True)
+    add_parser.add_argument("-i", "--icon", required=True)
+
+    # Removing entry
+
+    remove_parser = subparsers.add_parser(
+        "remove", help="Remove an existing desktop entry"
+    )
+
     args = parser.parse_args()
-    print(args.add)
+
+    if args.command == "add":
+        create_desktop_entry(args.name, args.exec, args.path, args.icon)
+
 
 if __name__ == "__main__":
     run()
---
+++ src/lib.py
@@ -0,0 +1,71 @@
+import typing
+import os
+import settings
+
+
+class PrintLog:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+    def success(self, message: str):
+        print(f"{self.OKGREEN}{message}")
+
+    def error(self, message: str):
+        raise Exception(f"{self.FAIL}{message}")
+
+    def warning(self, message: str):
+        print(f"{self.WARNING}{message}")
+
+
+def create_desktop_entry(
+    name: str,
+    exec: str,
+    path: str,
+    icon: str,
+    entry_filename: str = "",
+    version: str = "1.0",
+    terminal: bool = False,
+    type: str = "Application",
+    categories: typing.List[str] = ["Utility"],
+):
+    log = PrintLog()
+
+    if entry_filename == "":
+        entry_filename = f"{name.replace(" ", "_").lower()}.desktop"
+        log.warning(
+            f"No filename provided, generated from program name: {entry_filename}"
+        )
+
+    if entry_filename in os.listdir(settings.DESKTOP_ENTRY_LOCATION):
+        log.error(f"An entry with the same file name already exists: {entry_filename}")
+
+    terminal = "true" if terminal else "false"
+
+    entry = f""" 
+    [Desktop Entry]
+    Version={version}
+    Name={name}
+    Exec={exec}
+    Path={path}
+    Icon={icon}
+    Terminal={terminal}
+    Type={type}
+    Categories={create_categories_string(categories)}
+    """
+
+    with open(os.path.join(settings.DESKTOP_ENTRY_LOCATION, entry_filename), "w") as f:
+        f.write(entry)
+
+    log.success("Desktop entry created successfully!")
+
+
+def create_categories_string(categories: typing.List[str]):
+    categories_string = ";".join(categories)
+    return categories_string
